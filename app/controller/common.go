a6908ca15bbfe1094682d2e061fdcdbdf839eeea
moli <molix2@outlook.com>
2020-01-17 00:28:27 +0800

    update: 调整歧义代码

--- app/controller/common.go
+++ app/controller/common.go
@@ -10,9 +10,26 @@ const (
 	MaxGraphQLRequestBody = int64(1 << 20) // 1MB
 )
 
+const (
+	AuthUserToken = "auth-user-token"
+)
+
 func LimitGraphQLRequestBody(ctx *gin.Context) {
 	if ctx.Request.ContentLength > MaxGraphQLRequestBody {
 		ctx.AbortWithStatus(http.StatusRequestEntityTooLarge)
 		return
 	}
+}
+
+func GetUserToken(ctx *gin.Context) string {
+	token := getValueFromHeaderOrCookie(AuthUserToken, ctx)
+	return token
+}
+
+func getValueFromHeaderOrCookie(k string, ctx *gin.Context) string {
+	v := ctx.GetHeader(k)
+	if len(v) == 0 {
+		v, _ = ctx.Cookie(k)
+	}
+	return v
 }
