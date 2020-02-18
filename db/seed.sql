6e0f6029e9b21af18d43618b5e9bc6eeab1aa649
chanhx <ichan.hocheung@gmail.com>
2020-02-18 20:57:50 +0800

    fix: 修正表名，使得插入操作的表名与创建时一致
    reference: https://stackoverflow.com/questions/9814645

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,1 @@
-INSERT INTO server (summary, host, port, status, created_at, deleted_at) VALUES ('local', '127.0.0.1', 9000, 1, extract(epoch from now()), null);
+INSERT INTO public.server (summary, host, port, status, created_at, deleted_at) VALUES ('local', '127.0.0.1', 9000, 1, extract(epoch from now()), null);
