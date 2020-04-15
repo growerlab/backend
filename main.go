3a3e08b8a74a2de970cd9300da2219fcdab3004e
moli <molix2@outlook.com>
2020-04-16 00:56:10 +0800

    add: port

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/growerlab/backend/app"
 	"github.com/growerlab/backend/app/common/notify"
+	"github.com/growerlab/backend/app/utils/conf"
 	"github.com/growerlab/backend/app/utils/logger"
 )
 
 func main() {
-	err := app.Run(":8080")
+	addr := fmt.Sprintf(":%d", conf.GetConf().Port)
+	err := app.Run(addr)
 	if err != nil {
 		if err == http.ErrServerClosed {
 			logger.Info("bye.")
